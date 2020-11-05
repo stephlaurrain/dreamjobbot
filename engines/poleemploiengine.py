ef8b9b7bc2c4df13ca9c40860dc3e0604ba8534f
lor1cd <laurrains@gmail.com>
2020-11-05 18:23:51 +0100

    pe fonctionne

--- engines/poleemploiengine.py
+++ engines/poleemploiengine.py
@@ -23,6 +23,7 @@ class Poleemploiengine:
                 self.visitedthissession = list()
                 self.dumbthissession = list()
                 self.mainclass=mainclass
+                self.report=""                
 
         def dosearch(self, site, place, words):
                 self.mainclass.trace(inspect.stack()[0])          
@@ -39,25 +40,23 @@ class Poleemploiengine:
         def getads(self):
                 self.mainclass.trace(inspect.stack()[0])          
                 try:
-                        report =""
-                        mainlist = self.mainclass.driver.find_element_by_class_name("result")
-                        orgurl = mainlist.find_element_by_css_selector("a").get_attribute("href")
-                        print(orgurl)
-                        #
-                        report+=self.mainclass.htmlfactory.geturltolink(orgurl)
-                        mainlist.click()
-                        #titleel=self.mainclass.driver.find_element_by_css_selector("#detailOffreVolet > h2")
-                        #report+=titleel.get_attribute("innerhtml")
                         
-                        wait = WebDriverWait(self.mainclass.driver, 15)
-                        wait.until(EC.invisibility_of_element_located((By.ID, "loader-container")))
-                        adel = self.mainclass.driver.find_element_by_id("detailOffreVolet")
-                        #input ("press key : ")
-                        report+=adel.get_attribute("innerHTML")
-                        print(report)
-                        btnclose = self.mainclass.driver.find_element_by_css_selector("#PopinDetails > div > div > div > div.modal-header > div > button")
-                        btnclose.click()
-                        return report
+                        mainlist = self.mainclass.driver.find_elements_by_class_name("result")
+                        for res in mainlist:
+                                orgurl = res.find_element_by_css_selector("a").get_attribute("href")
+                                print(orgurl)
+                                #
+                                self.report+=self.mainclass.htmlfactory.geturltolink(orgurl)
+                                res.click()
+                                self.mainclass.waithuman()
+                                wait = WebDriverWait(self.mainclass.driver, 15)
+                                wait.until(EC.invisibility_of_element_located((By.ID, "loader-container")))
+                                adel = self.mainclass.driver.find_element_by_id("detailOffreVolet")
+                                #input ("press key : ")
+                                self.report+=adel.get_attribute("innerHTML")                                
+                                btnclose = self.mainclass.driver.find_element_by_css_selector("#PopinDetails > div > div > div > div.modal-header > div > button")
+                                btnclose.click()
+                                self.mainclass.waithuman()
                 except Exception as e:
                         self.mainclass.log.errlg(e)
                         raise
@@ -65,11 +64,9 @@ class Poleemploiengine:
         def getreport(self, site, place, words):
                 self.mainclass.trace(inspect.stack()[0])         
                 try:
-                    self.mainclass.log.lg("test")
-                    self.dosearch(site, place, words)
-                    report = self.getads()    
-                    return report
-                    #return (str(site))
+                    self.dosearch(site, place, words)  
+                    self.getads()              
+                    return self.report                    
                 except Exception as e:
                         self.mainclass.log.errlg(e)
                         raise
